be371451e1483b258ae3c0bcf926fe620071c056
Edmond <unstopableenergy@gmail.com>
2025-07-09 23:21:41 +0000

    complete bookstore app with paystack integration, dashboard , and sales tracking

--- obj/Debug/net8.0/BookStore.Api.AssemblyInfo.cs
+++ obj/Debug/net8.0/BookStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a30332798baaf11649f03fa05a72f19e46dd2af0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b579f608dd95e7497dbdec262defea585b2f03ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
